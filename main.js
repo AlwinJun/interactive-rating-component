c7e821bfd9d2e152909819a2b28964cc1d2ced98
AJ <mahidlawonaljun@gmail.com>
2022-09-22 20:38:07 +0800

    Put value on rate on thankyou card

--- main.js
+++ main.js
@@ -6,26 +6,33 @@ const rateBtns = document.querySelectorAll('.btns')
 const rate = document.querySelector('.thankyou-rate span')
 
 
-rateBtns.forEach(rateBtn => {
+// Give click event to all rateBtns
+// Get and store the button value in rate
+rateBtns.forEach((rateBtn) => {
   rateBtn.addEventListener('click', () => {
 
-  console.log(rateBtn.textContent)
-
+    rate.textContent = rateBtn.textContent
+    
   })
 })
+
+//If the user didn't pick the rate will defaulted to 5
+rate.textContent = 5
 
 // When click: hide submit card, show thankyou card
 submit.addEventListener('click', function() {
 
   submitCard.style.display = 'none'
   thankYouCard.classList.remove("hide")
+  
 
 })
 
-// When click: hide thankyou card show submit card 
+//When click: hide thankyou card show submit card 
 goBack.addEventListener('click', function() {
 
   thankYouCard.classList.add('hide')
   submitCard.style.display = 'block'
 
 })
+
