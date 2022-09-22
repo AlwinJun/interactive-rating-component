a1f2e52b7084e6ef9e33d5aec2bbc869ef445f47
AJ <mahidlawonaljun@gmail.com>
2022-09-22 11:27:53 +0800

    Add event for submit and go back button

---
+++ main.js
@@ -0,0 +1,20 @@
+const submit = document.getElementById('submit')
+const goBack = document.getElementById('go-back')
+const submitCard = document.querySelector('.submit-card')
+const thankYouCard = document.querySelector('.thankyou-card')
+
+// When click: hide submit card, show thankyou card
+submit.addEventListener('click', function(){
+
+  submitCard.style.display = 'none'
+  thankYouCard.classList.remove("hide")
+
+})
+
+// When click: hide thankyou card show submit card 
+goBack.addEventListener('click', function(){
+
+  thankYouCard.classList.add('hide')
+  submitCard.style.display = 'block'
+
+})
